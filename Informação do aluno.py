7f1c5f845d62786f0173d259064c15cdc5053d74
Jhonas Santos <patinhuh01@gmail.com>
2024-09-07 16:24:17 -0300

    terminado

--- Informação do aluno.py
+++ Informação do aluno.py
@@ -1,18 +1,26 @@
 turma = []
 aluno = {}
-nota = 'notas'
-print("Registro de Alunos")
+nota = 'Notas'
+
+def calculo(x):
+    total = sum(x)
+    media = total / len(aluno[nota])
+    return media
+
+print("=+"*30, "Registro de Alunos", "=+"*30)
 while True:
-    aluno['nome'] = str(input("Nome do aluno: "))
-    aluno['idade'] = int(input("Idade do aluno: "))
+    aluno['Nome'] = str(input("Nome do aluno: "))
+    aluno['Idade'] = int(input("Idade do aluno: "))
+
     for cont in range(0, 4):
         notas = float(input("Notas do aluno: "))
         aluno.setdefault(nota, [])
         aluno[nota].append(notas)
+    aluno['Media'] = calculo (aluno[nota])
     turma.append(aluno.copy())
     escolha = str(input("Gostaria de registrar mais algum aluno? [S/N]: ")).lower()
+    
     if escolha != "s":
         break
 
-for a in turma:
-    print(a)
+
--- a = {}.py
+++ a = {}.py
@@ -6,7 +6,12 @@ for cont in range(0, 4):
     notas = float(input("Notas do aluno: "))
     aluno.setdefault(nota, [])
     aluno[nota].append(notas)
+ 
 
+def calculo(x):
+    total = sum(x)
+    media = total / len(aluno[nota])
+    return media
 
-
-print(aluno)
+media = calculo (aluno[nota])
+print(media)
