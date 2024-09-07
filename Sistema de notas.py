c67661ac608d897aa2315ad3b618a237551ab2b4
Jhonas Santos <patinhuh01@gmail.com>
2024-09-07 16:38:38 -0300

    Update Sistema de notas.py

--- Sistema de notas.py
+++ Sistema de notas.py
@@ -1,17 +1,28 @@
 # Definindo a Var
 notas = []
 
+# Loop para descrever as notas
 for (i) in range(4):
     x = float(input("Digite a nota: "))
     notas.append(x)
 
+# Função que realizará o calculo de média 
 def calcular_media(notas):
     total = sum(notas)
     media = total/len(notas)
     return media
 
-media = calcular_media(notas)
+# Função anônima para arrendar a media em duas casas decimais.
+arredondar_media = lambda media: round(media, 2)
 
+# Calcular a média.
+media = calcular_media(notas)
+media_arredondada = arredondar_media(media)
+
+# Exibição das notas e sua média final.
+print (f"Notas do aluno: {notas}\n",end= ""f"Média final do aluno: {media}")
+
+# Situação do aluno.
 if media >= 7:
     print(f'\nO aluno foi aprovado com uma média de {media}')
 else:
