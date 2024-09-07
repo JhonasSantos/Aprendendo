2a67433bb62fc566f90ea1cd10b6b52176a02e99
Jhonas Santos <patinhuh01@gmail.com>
2024-09-07 13:06:13 -0300

    Registrando informações em dicionarios e passando para uma lista

--- Informação do aluno.py
+++ Informação do aluno.py
@@ -1,10 +1,14 @@
 turma = []
 aluno = {}
 
+print("Registro de Alunos")
 while True:
     aluno['nome'] = str(input("Nome do aluno: "))
     aluno['idade'] = int(input("Idade do aluno: "))
-    turma.append(aluno)
-    escolha = str(input("Gostaria de adicionar outro aluno? [S/N]: ")).lower()
-    if escolha != 's':
+    turma.append(aluno.copy())
+    escolha = str(input("Gostaria de registrar mais algum aluno? [S/N]: ")).lower()
+    if escolha != "s":
         break
+
+for a in turma:
+    print(a)
